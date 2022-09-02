c0bd90d21915c186bdde5ce2559b89b5c407ccf6
Nikolay <n.astafyev@yandex.ru>
2022-09-03 00:39:18 +0800

    add way 2

--- mission25/Program.cs
+++ mission25/Program.cs
@@ -4,13 +4,30 @@
 
 // 2, 4 -> 16
 
+// way 1
+// Console.Write("Введите число: ");
+// double num = Convert.ToDouble(Console.ReadLine());
+
+// Console.Write("Введите натуральную степень: ");
+// int degree = Convert.ToInt32(Console.ReadLine());
+
+// if (degree>0 && degree % 1 == 0) Console.WriteLine("Результат: " + Math.Pow(num,degree));
+
+// else Console.WriteLine("Вы ввели ненатуральную степень");
+
+// way2
 
 Console.Write("Введите число: ");
 double num = Convert.ToDouble(Console.ReadLine());
 
 Console.Write("Введите натуральную степень: ");
-double degree = Convert.ToDouble(Console.ReadLine());
+int degree = Convert.ToInt32(Console.ReadLine());
 
-if (degree>0 && degree % 1 == 0) Console.WriteLine("Результат: " + Math.Pow(num,degree));
+double Product(double number, int count)
+{   
+    double result = number;
+    for (int i = 1; i < count; i++) result= result * number;
+    return result;
+}
 
-else Console.WriteLine("Вы ввели ненатуральную степень");
+Console.WriteLine("Результат: " + Product(num, degree));
